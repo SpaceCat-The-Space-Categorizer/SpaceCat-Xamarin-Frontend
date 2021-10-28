50f3134fcad353de4f1b5b1255e02372192b254a
Autumn <ao191@humboldt.edu>
2021-10-27 19:25:09 -0700

    Added BuildingListViewModel binding, began implementing MVVM model

---
+++ SpaceCat-Xamarin-Frontend/SpaceCat-Xamarin-Frontend/BuildingListViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SpaceCat_Xamarin_Frontend
+{
+    public class BuildingListViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand GoToSurveyMode => new Command(Clicked_Survey);
+
+        // SELECTED BUILDING VARIABLES
+        private string _name;
+        private string _date;
+        private string _floorplan;
+        private string _status;
+        public string Name 
+        { get { return _name; }
+            set 
+            {
+                _name = value;
+                OnPropertyChanged();
+            } }
+        public string Date
+        {
+            get { return _date; }
+            set
+            {
+                _date = value;
+                OnPropertyChanged();
+            }
+        }
+        public string FloorPlan
+        {
+            get { return _floorplan; }
+            set
+            {
+                _floorplan = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Status
+        {
+            get { return _status; }
+            set
+            {
+                _status = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // BUILDING LIST VARIABLE
+        public ObservableCollection<string> Buildings { get; set; }
+
+        public BuildingListViewModel()
+        {
+            Buildings = new ObservableCollection<string>();
+
+            Buildings.Add("Building 1");
+            Buildings.Add("Building 2");
+            Buildings.Add("Building 3");
+            Buildings.Add("Building 4");
+        }
+
+        private void Clicked_Survey()
+        {
+            // ALTER ON IMPLEMENT - need Building class, placeholder data to check binding
+            this.Name = "Building Name";
+            this.Date = "March 2022";
+            this.FloorPlan = "*floorplan image*";
+            this.Status = "Status: Ready for Survey";
+
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- SpaceCat-Xamarin-Frontend/SpaceCat-Xamarin-Frontend/MainPage.xaml.cs
+++ SpaceCat-Xamarin-Frontend/SpaceCat-Xamarin-Frontend/MainPage.xaml.cs
@@ -4,15 +4,34 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace SpaceCat_Xamarin_Frontend
 {
     public partial class MainPage : ContentPage
     {
+        public ICommand TapCommand => new Command<string>(SelectBuilding);
+
+        
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        void SelectBuilding(string build)
+        {
+
+        }
+
+        private void Clicked_Analysis(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Clicked_Create(object sender, EventArgs e)
+        {
+
         }
     }
 }
