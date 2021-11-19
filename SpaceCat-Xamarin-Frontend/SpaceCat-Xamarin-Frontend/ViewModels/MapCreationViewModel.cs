c7f344c1342b296164f41befc715950b969b718a
Ryan Beck <rb200@humboldt.edu>
2021-11-18 23:25:52 -0800

    workin on creating local version of file

--- SpaceCat-Xamarin-Frontend/SpaceCat-Xamarin-Frontend/ViewModels/MapCreationViewModel.cs
+++ SpaceCat-Xamarin-Frontend/SpaceCat-Xamarin-Frontend/ViewModels/MapCreationViewModel.cs
@@ -288,7 +288,7 @@ namespace SpaceCat_Xamarin_Frontend
         private async void ExecuteMapSettings(object s)
         {
             // Handles tapping the map settings button (unimplemented)
-            System.Diagnostics.Debug.WriteLine("Tapped Settings!");
+            System.Diagnostics.Debug.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
 
             // open file picker
             try
@@ -300,8 +300,46 @@ namespace SpaceCat_Xamarin_Frontend
                 });
                 if (result != null)
                 {
-                    MapImage = ImageSource.FromFile(result.FullPath);
-                   
+                    //MapImage = ImageSource.FromFile(result.FullPath);
+                    try
+                    {
+
+                        using (FileStream fsSource = new FileStream(result.FullPath,
+                            FileMode.Open, FileAccess.Read))
+                        {
+
+                            // Read the source file into a byte array.
+                            byte[] bytes = new byte[fsSource.Length];
+                            int numBytesToRead = (int)fsSource.Length;
+                            int numBytesRead = 0;
+                            while (numBytesToRead > 0)
+                            {
+                                // Read may return anything from 0 to numBytesToRead.
+                                int n = fsSource.Read(bytes, numBytesRead, numBytesToRead);
+
+                                // Break when the end of the file is reached.
+                                if (n == 0)
+                                    break;
+
+                                numBytesRead += n;
+                                numBytesToRead -= n;
+                            }
+                            numBytesToRead = bytes.Length;
+
+
+
+                            // Write the byte array to the other FileStream.
+                            //using (FileStream fsNew = new FileStream(pathNew,
+                            //    FileMode.Create, FileAccess.Write))
+                            //{
+                            //    fsNew.Write(bytes, 0, numBytesToRead);
+                            //}
+                        }
+                    }
+                    catch (FileNotFoundException ioEx)
+                    {
+                        Console.WriteLine(ioEx.Message);
+                    }
 
                 }
             }
